4f2155c04c29cdcfe4498eb9a7dddb610b221f88
PrashantMohta <mohtaprashantrocks@gmail.com>
2018-07-03 14:38:02 +0530

    starting a conversation

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import {SpeechRecognition,speechSynthesis,SpeechSynthesisUtterance} from './Spee
 
 class SpeechComponent extends Component
 {
+
+state={recognitionEnded:false,speaking:false}
 recognition = new SpeechRecognition();
+synth = speechSynthesis;
+utterThis = new SpeechSynthesisUtterance("");
 
 componentWillMount = () =>{
   this.recognition.lang = 'en-US';
@@ -18,38 +22,51 @@ componentWillMount = () =>{
     var recognised = event.results[last][0].transcript;
     //console.log(event.results[last][0]);
     console.log('recognised : '+recognised+'\n Confidence: ' + event.results[0][0].confidence);
-    this.props.callback(recognised,event.results[0][0].confidence);
+    this.props.recognitionCallback(recognised,event.results[0][0].confidence);
   }
-  this.recognition.onend = function() {
+  this.recognition.onend = () => {
     console.info("voice recognition ended...");
+    this.setState({recognitionEnded:true})
+  }
 }
 
-}
 componentDidMount =() =>{
-  this.Speak(this.props.label);
+  this.props.allowSpeak(this.Speak); 
   this.props.allowRecognition(this.startRecognition); 
-  this.startRecognition();
+  //this.Speak(this.props.label);
+  //this.startRecognition();
 }
-componentDidUpdate = () =>{
-  this.Speak(this.props.label);
-}
+//componentDidUpdate = () =>{
+  //this.Speak(this.props.label);
+//}
 
 startRecognition = () => {
   this.recognition.start();
+  this.setState({recognitionEnded:false})
   console.log('Ready to receive a  command.');
 }
 
 Speak=(text) => {
-  var synth = speechSynthesis;
-  var utterThis = new SpeechSynthesisUtterance(text);
-  synth.speak(utterThis);
+  this.utterThis = new SpeechSynthesisUtterance(text);
+  this.synth.speak(this.utterThis);
+  this.setState({speaking:true})
+  this.utterThis.onend = () =>{ 
+    console.info("speech ended...");
+    this.setState({speaking:false})
+    this.startRecognition();
+  }
 }
 
   render()
   {
+    let button;
+    if(this.state.recognitionEnded && !this.state.speaking)
+    button= <button onClick={this.startRecognition}> restart recognition </button>;
+    
     return(
     <div className="controls">
         {this.props.label}
+        {button}
     </div>
   );}
 }
@@ -57,14 +74,19 @@ Speak=(text) => {
 class App extends Component {
   state={label:`which of these cuties will be your starter pokemon ! \n will it be squirtle? charmander? or bulbasaur? `,
         responses:["squirtle","charmander","bulbasaur"]};
+
   startRecognition=()=>{console.log("undefined recognition")}
+  speak=()=>{console.log("undefined speak")}
+
+  start=setTimeout(()=>{this.speak(this.state.label)},500);
   userSays=(words,confidence)=>{
     if(this.state.responses.indexOf(words)===-1)
     {
       this.setState({label:`I'm not sure i understand,  "${words}" is not a valid choice! \n just speak the pokemon's name.`});
-      setTimeout(this.startRecognition,500);
+      this.speak(this.state.label)
     }else{
       this.setState({label:` "${words}" is a great choice !`});
+      this.speak(this.state.label)
     }
     console.log(`words ${words} with confidence ${confidence}`);
   }
@@ -85,8 +107,14 @@ class App extends Component {
           <img className="pokemon charmander" src="images/charmander_front.png" alt="bulbasaur"/>
         </div>
       </div>
-      <SpeechComponent label={this.state.label} callback={this.userSays} allowRecognition={startRecognition => this.startRecognition =startRecognition}></SpeechComponent>
-      <button onClick={this.startRecognition}> Text</button>
+
+      <SpeechComponent label={this.state.label} 
+      recognitionCallback={this.userSays} 
+      allowRecognition={startRecognition => {this.startRecognition =startRecognition} } 
+      allowSpeak={Speak => {this.speak =Speak;}}>
+      </SpeechComponent>
+      <button onClick={()=>{this.speak(this.state.label)}}>speak</button>
+
       </div>
     );
   }
